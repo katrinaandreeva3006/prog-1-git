8b92339433a73c05e68d47641a9fb1ac5038863b
katrinaandreeva3006 <77636957+katrinaandreeva3006@users.noreply.github.com>
2021-02-25 19:25:54 +0200

    Add files via upload

---
+++ darbs.py
@@ -0,0 +1,20 @@
+a = 5
+b = 15
+c = 10
+
+if b > a:
+ print("b ir lielaks par a")
+else:
+ print("b ir lielaks par a")
+if c < b:
+ print("c ir mazaks par b")
+else:
+ print("c ir mazaks par b")
+if c > a:
+ print("c ir lielaks par a")
+else:
+ print("c ir lielaks par a")
+if b > a/c:
+ print("b ir visslielaks")
+else:
+ print("b ir visslielaks")
